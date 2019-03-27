9460a1e17a00bb156159384dff59de169fd746a4
André Martins <48385510+141Hash@users.noreply.github.com>
2019-03-27 11:26:32 +0000

    Create 14.c

---
+++ 14.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int maxInArray (int *ascii, int n){
+    int i = 0;
+    int j = 0;
+    int max = ascii[i];
+    for (i = 0; i < n; i++){
+        if (ascii[i] > max){
+            max = ascii[i];
+            j = i;
+        }
+    }
+    return j;
+}
+
+char charMaisfreq (char s[]) {
+    int ascii[127] = {0};
+    int i;
+    if (s[0] == '\0'){
+        return 0;
+    }
+    for (i = 0; s[i] != '\0'; i++){
+        ascii[s[i]] += 1;
+    }
+    return maxInArray (ascii, 127);
+}
