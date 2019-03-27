ca7423fdd91b16aebd44d84600be5ba7816303ed
André Martins <48385510+141Hash@users.noreply.github.com>
2019-03-27 11:21:14 +0000

    Create 004.c

---
+++ 004.c
@@ -0,0 +1,12 @@
+int bitsUm (int x){
+    int r=0;
+    while(x != 0){
+        if(x % 2 == 0){
+            x /= 2;
+        } else {
+            x /= 2;
+            r++;
+        }
+    }
+    return r;
+}
