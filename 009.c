5df0ab4eb07e38dfc81f58aa33792c3eaa81e143
André Martins <48385510+141Hash@users.noreply.github.com>
2019-06-16 20:22:59 +0100

    Update 009.c

--- 009.c
+++ 009.c
@@ -1,34 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 
-int strcmp1 (char s1[], char s2[]){
-    int x, z;
-    for (x = 0; s1[x] != '\0'; x++){
-
-        if (s1[x] != '\0' && s2[x] == '\0'){
-            z = 1;
-            break;
-        }
-        if (s1[x] > s2[x]){
-            z = 1;
-            break;
-        }
-        if (s1[x] < s2[x]){
-            z = -1;
-            break;
-        }
-
-    }
-
-    if (s1[x] == '\0' && s2[x] == '\0'){
-        z = 0;
-
-    }
-
-    if (s1[x] == '\0' && s2[x] != '\0'){
-        z = -1;
-
-    }
-
-    return z;
+int mystrcmp(char s1[], char s2[]) {
+    int i;
+    for(i = 0; s1[i] == s2[i] && s1[i] != '\0';i++);
+    return(s1[i] - s2[i]);
 }
