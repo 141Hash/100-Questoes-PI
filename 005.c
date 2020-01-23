a00356e8439a81c96051785e1ca40f1d31f1246f
André Martins <48385510+141Hash@users.noreply.github.com>
2020-01-23 23:51:39 +0000

    Update 005.c

--- 005.c
+++ 005.c
@@ -3,17 +3,10 @@
 
 int trailingZ (int n) {
     int numberOfZeros = 0;
-    if (n % 2 == 0) {
-        while (n / 2 != 0) {
-           if (n % 2 == 0) {
-            n = n / 2;
-            numberOfZeros ++;
-           }
-           else {
-            break;
-           }
-        };
+    while (n % 2 == 0){
+        n /= 2;
+        numberOfZeros++;
+        
     }
-    else numberOfZeros = 0;
     return numberOfZeros;
 }
